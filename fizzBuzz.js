d7cb504342c642f850a254d4da72e33417c109bd
henshaw rowland <henshawrowland@yahoo.com>
2017-01-07 10:33:10 +0100

    Corrected fizzbuzz function

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,5 +1,6 @@
-function fizzBuzz(n) {
+exports.fizzBuzz = function(n) {
 	if (n % 3 === 0 && n % 5 === 0) { return 'FizzBuzz'}
 	else if (n % 3 === 0) { return 'Fizz'; }
 	else if (n % 5 === 0) { return'Buzz'; }
+	return n;
 }
