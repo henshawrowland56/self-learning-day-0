c0117d60178edd8e326a6337ef228d4aef6f99ea
henshaw rowland <henshawrowland@yahoo.com>
2017-01-07 09:42:16 +0100

    Added fourth test case to tests.js file

--- tests.js
+++ tests.js
@@ -29,6 +29,14 @@
         expect(findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
       });
 
+      it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]', function () {
+        expect(findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
+      });
+
+      it('should return [10, 20] for [11, 13, 18, 20, 19, 10, 11, 14]', function () {
+        expect(findMinMax([11, 13, 18, 20, 19, 10, 11, 14])).toEqual([10, 20]);
+      });
+
     });
 
     describe('Return the number in the list in a new list follows `[min]` when the number is the min and max number in that list', function () {
